b3871333ae311fcabce8647162a624e14d70ab70
Kristen <kriscadacio@gmail.com>
2023-03-29 22:29:36 -0400

    Added new variables to collect inputs

--- coffee.py
+++ coffee.py
@@ -2,19 +2,31 @@ from main import MENU, resources
 
 espresso_info = MENU["espresso"]
 latte_info = MENU["latte"]
-cappuccino = MENU["cappuccino"]
-
-
-user_order = input("What would you like? (espresso/latte/capuccino): ")
+cappuccino_info = MENU["cappuccino"]
 
 user_pays = 0
 
 #RESOURCE TRACKING
 total_profit = 0
 
+user_order = input("What would you like? (espresso/latte/capuccino): ")
+
 if user_order == "espresso":
     user_pays = espresso_info["cost"]
 elif user_order == "latte":
-    user_pays == latte_info["cost"]
+    user_pays = latte_info["cost"]
+elif user_order == "cappucino":
+    user_pays = cappuccino_info["cost"]
+elif user_order == "report":
+    user_pays = 1000
 
-print(user_pays)
+
+
+print("Please insert coins.")
+quarters = input("how many quarters?: ")
+dimes = input("how many dimes?: ")
+nickles = input("how many nickles?: ")
+pennies = input("how many pennies?: ")
+
+
+
